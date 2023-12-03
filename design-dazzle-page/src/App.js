00a08505fda1cbad817d0c83a1add9c729e98823
byunn90 <kayhanturk90@Live.com>
2023-12-03 19:38:58 +1100

    ,

---
+++ design-dazzle-page/src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/footer";
+
+export default function App() {
+  return (
+    <div>
+      <footer />
+    </div>
+  );
+}
