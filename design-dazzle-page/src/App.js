d3fd69b80b7addf33e411b7d665928a9431ee590
byunn90 <kayhanturk90@Live.com>
2023-12-26 19:33:01 +1100

    starter

--- design-dazzle-page/src/App.js
+++ design-dazzle-page/src/App.js
@@ -1,11 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Footer from "./components/footer";
+import pic from "./images/usama-akram-kP6knT7tjn4-unsplash.jpg";
+import "./styles.css";
 
 export default function App() {
   return (
     <div>
-      <footer />
+      <NavBar />
+      <Logo />
+      <Footer />
     </div>
   );
+}
+
+function Logo() {
+  return (
+    <div>
+      <img className="logo" src={pic} alt="logo" />
+    </div>
+  );
+}
+
+function NavBar() {
+  return <header>HII TODO</header>;
+}
+
+function Footer() {
+  return <footer>Hi</footer>;
 }
